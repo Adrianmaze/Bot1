e076a7515dc4c8ebb94157fc39f1b7a272d89de0
Adrianmaze <anuardavid2728@gmail.com>
2024-12-04 16:28:32 -0400

    Create fun-kiss.js

---
+++ plugins/fun-kiss.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+// Lista de videos (asegúrate de que estén en la ruta correcta)
+const videoList = [
+    'https://telegra.ph/file/ba841c699e9e039deadb3.mp4',  // Ajusta las rutas de tus videos
+    'https://telegra.ph/file/8c0f70ed2bfd95a125993.mp4',
+    'https://telegra.ph/file/0fc525a0d735f917fd25b.mp4'
+];
+
+// Escuchar mensajes
+client.on('message', async (message) => {
+    // Verificar si el mensaje es el comando ".besar"
+    if (message.body.toLowerCase() === '.besar') {
+        // Escoger un video aleatorio de la lista
+        const randomVideo = videoList[Math.floor(Math.random() * videoList.length)];
+
+        // Enviar un mensaje con el video aleatorio
+        const chat = await message.getChat();
+        chat.sendMessage('¡Te mando un beso! 💋', { media: fs.createReadStream(randomVideo) });
+    }
+}); 
