c02ea7e60ab6ae19c820faf3797ec4a50c9cf09e
Adrianmaze <anuardavid2728@gmail.com>
2024-12-09 23:30:13 -0400

    Update fun-penetrar.js

--- plugins/fun-penetrar.js
+++ plugins/fun-penetrar.js
@@ -1,17 +1,32 @@
 let handler = async (m, { conn, command, text }) => {
-if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-conn.reply(m.chat, `
-*TE HAN LLENADO LA CARA DE SEMEN POR PUTA Y ZORRA!*
+    // Validar si hay una mención
+    if (!m.mentionedJid || !m.mentionedJid[0]) {
+        throw `*Debes mencionar a alguien usando @ o responder a un mensaje si quieres saber si te puedes ${command.replace('how', '')}*`;
+    }
 
-*le ha metido el pene a ${text}* con todo y condon hasta quedar seco, has dicho "por favor mas duroooooo!, ahhhhhhh, ahhhhhh, hazme un hijo que sea igual de pitudo que tu!" mientras te penetraba y luego te ha dejado en silla de ruedas!
+    let user = m.mentionedJid[0]; // Captura el usuario mencionado
+    let name = text || `@${user.split('@')[0]}`; // Obtiene el nombre o el @ del usuario
 
-*${text}* 
-🔥 *YA TE HAN PENETRADO!* `, null, { mentions: [user] })
-}
-handler.help = ['penetrar @user']
-handler.tags = ['fun']
-handler.command = ['penetrar','penetrado']
+    conn.reply(
+        m.chat,
+        `
+*¡TE HAN LLENADO LA CARA DE SEMEN POR PUTA Y ZORRA!*
+
+*Le han metido el pene a ${name}* con todo y condón hasta quedar seco, has dicho: 
+"¡Por favor más duroooooo!, ahhhhhhh, ahhhhhh, hazme un hijo que sea igual de pitudo que tú!" 
+mientras te penetraba y luego te ha dejado en silla de ruedas.
+
+*${name}* 
+🔥 *¡YA TE HAN PENETRADO!*`,
+        null,
+        { mentions: [user] } // Menciona al usuario
+    );
+};
+
+handler.help = ['penetrar @user'];
+handler.tags = ['fun'];
+handler.command = ['penetrar', 'penetrado'];
 handler.group = true;
-handler.fail = null
-export default handler
+handler.fail = null;
+
+export default handler;
